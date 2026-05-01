32cfa14a180b8ab3324721dbeb57bf79f4b98b2e
Семён <semyon.lyam@gmail.com>
2026-05-01 21:54:04 +0200

    Step 0 Done | add TicketOrder tests

---
+++ tests/TicketOrderTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class TicketOrderTest extends TestCase
+{
+    public function testTotalPriceStandardWithoutGlasses()
+    {
+        $order = new TicketOrder('Semyon', 2, 'Interstellar', false, 'standard');
+        $this->assertEquals(800, $order->getTotalPrice());
+    }
+
+    public function testTotalPriceVipWithGlasses()
+    {
+        $order = new TicketOrder('Semyon', 3, 'Dune', true, 'vip');
+        $this->assertEquals(1950, $order->getTotalPrice());
+    }
+
+    public function testInvalidTicketCount()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new TicketOrder('Semyon', 0, 'Dune', false, 'standard');
+    }
+}
